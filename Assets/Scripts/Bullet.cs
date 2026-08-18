8a44262e4bc73bc63bbcaac96da7892830d93460
Jerecraft <contactojerecraft@gmail.com>
2026-08-18 16:55:30 -0500

    bala agregada

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,8 +3,34 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
 {
+    public int damageBullet;
+    public float BulletSpeed = 3f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.linearVelocity = transform.up * BulletSpeed;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.transform.CompareTag("Enemy"))
+        {
+            HitEnemy(collision);
+        }
     }
+
+    private void HitEnemy(Collider2D actualEnemy)
+    {
+        //codigo cuando choque con enemigo
+        Enemy enemy = actualEnemy.GetComponent<Enemy>();
+
+        enemy.TakeDamage(damageBullet);
+
+        Destroy(gameObject);
+    }
+
 }
